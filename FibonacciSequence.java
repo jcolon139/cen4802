109367f1d8f15fd097d32a2c9af3243e73c639fb
Javier <jcolon139@yahoo.com>
2023-01-21 23:05:53 -0500

    first commit

---
+++ FibonacciSequence.java
@@ -0,0 +1,18 @@
+public class FibonacciSequence {
+    public static void main(String[] args) {
+    int fInt = 10;
+        System.out.print("The " + fInt + "th term of the Fibonacci sequence is: " + FibonacciTest.recursiveFibonacci(fInt));
+
+    }
+}
+class FibonacciTest {
+    public static int recursiveFibonacci(int n) {
+        if(n <= 1)
+        {
+            return 1;
+        }
+        else {
+            return recursiveFibonacci(n - 1) + recursiveFibonacci(n - 2);
+        }
+    }
+}
