f86e446c57ca9314c26eab969a4db459e1c44929
Javier <jcolon139@yahoo.com>
2023-01-22 02:18:25 -0500

    Initial Commit
    Signed-off-by: Javier <jcolon139@yahoo.com>

--- FibonacciSequence.java
+++ FibonacciSequence.java
@@ -1,21 +1,14 @@
+
 public class FibonacciSequence {
 
     public static void main(String[] args) {
 
-    int fInt = 10;
+    int fiboInt = 10;
 
-        System.out.print("The " + fInt + "th term of the Fibonacci sequence is: " + recursiveFibonacci(fInt));
+        System.out.print("The " + fiboInt + "th term of the Fibonacci sequence is: " + recursiveFibonacci(fiboInt));
 
     }
 
-    /**
-     * This is the Recursive Fibonacci sequence program that will use a given integer as an argument
-     * to calculate its place in the Fibonacci sequence.
-     * @author Javier Colon Morales
-     * @param n the given integer to use as an argument in the calculation for its place in the
-     *          Fibonacci sequence.
-     * @return  returns the calculated Fibonacci sequence term using a given parameter greater than 1.
-     */
     public static int recursiveFibonacci(int n) {
             if(n <= 1)
             {
